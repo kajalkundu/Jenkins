90380660c4fb50c52bc6abe62a5fef28fefd8894
kajalk <kajal.kundu@gmail.com>
2016-07-21 11:35:47 +0100

    Updated with new value

--- HelloJava.java
+++ HelloJava.java
@@ -3,12 +3,12 @@ public class HelloJava {
 public static void main(String[] args)
  {
   
-  System.out.println("This a small Java Program");
+  System.out.println("This is a small Java Program");
+  System.out.print("\n");
   
-  
-  for(int j=0;j<9;j++)
+  for(int j=0;j<50;j++)
   {
-	  for(int k=j;k<9;k++)
+	  for(int k=j;k<50;k++)
 	  {
 		  System.out.print(" * ");
 	  }
